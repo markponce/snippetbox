3df5822dd22a06d1c10d7e1d9039a6613f2f8881
Mark Anthony Ponce <markponce07@gmail.com>
2025-02-17 12:05:29 +0800

    Add separate route file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,15 +24,7 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	mux := app.routes()
 
 	logger.Info("start server", "addr", *addr)
 
