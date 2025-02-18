ef2f0977ecf2cca2597840b0c5d88ada85d65819
Mark Anthony Ponce <markponce07@gmail.com>
2025-02-18 11:22:48 +0800

    implement insert

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -18,9 +18,21 @@ type SnippetModel struct {
 }
 
 // insert
-func (m *SnippetModel) Insert(title string, content string, expires string) (int, error) {
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
-	return 0, nil
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
