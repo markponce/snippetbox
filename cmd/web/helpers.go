5a57a0183754d99a74de7e24ecd752b1c4c7eb10
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-12 18:03:57 +0800

    Add Flash field in newTemplateData constructor

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -30,6 +30,7 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
@@ -56,6 +57,8 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		app.serverError(w, r, err)
 		return
 	}
+
+	w.Header().Set("Content-Length", "this isn't an integer!")
 
 	// If the template is written to the buffer without any errors, we are safe
 	// to go ahead and write the HTTP status code to http.ResponseWriter.
