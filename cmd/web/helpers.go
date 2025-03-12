3d3ecedd5e00ddb25698d25924e0f43f0fd44d40
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-12 18:14:23 +0800

    Remove test error

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -58,8 +58,6 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		return
 	}
 
-	w.Header().Set("Content-Length", "this isn't an integer!")
-
 	// If the template is written to the buffer without any errors, we are safe
 	// to go ahead and write the HTTP status code to http.ResponseWriter.
 	w.WriteHeader(status)
