cf2f6b786047710abacb33267a5f6b4f1d657794
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-16 11:30:43 +0800

    Add IsAuthenticated field and function

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -29,8 +29,9 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -87,4 +88,10 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+// Return true if the current request is from an authenticated user, otherwise
+// return false.
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
