51e5727076ddfe0bed67bf5884cc1b46c732e048
Mark Anthony Ponce <markponce07@gmail.com>
2025-03-19 13:30:58 +0800

    Add about page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -29,6 +29,7 @@ func (app *application) routes() http.Handler {
 	// Use the nosurf middleware on all our 'dynamic' routes.
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /about/{$}", dynamic.ThenFunc(app.about))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
